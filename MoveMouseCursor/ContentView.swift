7c03ea67380f7fcfd736817cef963eb352a19500
big-jump <big_jump@mail.ru>
2023-08-22 00:42:13 +0400

    сommit

--- MoveMouseCursor/ContentView.swift
+++ MoveMouseCursor/ContentView.swift
@@ -3,5 +3,6 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         MouseCursorView()
+            .frame(width: 300, height: 200)
     }
 }
--- MoveMouseCursor/MouseCursorView.swift
+++ MoveMouseCursor/MouseCursorView.swift
@@ -1,7 +1,6 @@
 import SwiftUI
 
 struct MouseCursorView: View {
-    @State private var cursorPosition: CGPoint = .zero
     @State private var isMoving = false
     @State private var moveCount = 0
     @State private var nextMoveIn: Double = 5.0
@@ -38,23 +37,26 @@ struct MouseCursorView: View {
             }
         }
         .padding()
+        .frame(width: 300, height: 200)
     }
     
     private func moveCursor() {
         timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { timer in
             nextMoveIn -= 0.01
             if nextMoveIn <= 0 {
-                self.moveCursorByOnePixel()
+                self.moveCursorRandomly()
                 self.moveCount += 1
                 nextMoveIn = 5.0
             }
         }
     }
     
-    private func moveCursorByOnePixel() {
+    private func moveCursorRandomly() {
         let currentPos = NSEvent.mouseLocation
-        let newPos = CGPoint(x: currentPos.x + 1, y: currentPos.y)
+        let xOffset = Int.random(in: -1...1)
+        let yOffset = Int.random(in: -1...1)
+        let newPos = CGPoint(x: currentPos.x + CGFloat(xOffset), y: currentPos.y + CGFloat(yOffset))
         CGDisplayMoveCursorToPoint(0, newPos)
-        usleep(1000) // Add a small delay to ensure accurate 1-pixel movement
+        usleep(1000) // Add a small delay to ensure accurate movement
     }
 }
