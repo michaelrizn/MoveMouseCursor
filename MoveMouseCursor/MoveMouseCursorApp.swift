430270f2bfaf70ddfc3d4f0158a94054cf03e00a
big-jump <big_jump@mail.ru>
2023-08-21 23:44:49 +0400

    Initial Commit

---
+++ MoveMouseCursor/MoveMouseCursorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveMouseCursorApp.swift
+//  MoveMouseCursor
+//
+//  Created by Mikhail Ryazanov on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoveMouseCursorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
